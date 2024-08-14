278f7283a3c85ef54f7106ce297fcc01cb07bcdb
Mostafa Abogazia <aa7882784@gmail.com>
2024-08-14 03:39:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuToggle = document.getElementById('menuToggle');
+//     const navLinks = document.getElementById('navLinks');
+//     const closeBtn = document.getElementById('closeBtn');
+
+//     menuToggle.addEventListener('click', () => {
+//         navLinks.classList.add('nav-active');
+//     });
+
+//     closeBtn.addEventListener('click', () => {
+//         navLinks.classList.remove('nav-active');
+//     });
+// });
